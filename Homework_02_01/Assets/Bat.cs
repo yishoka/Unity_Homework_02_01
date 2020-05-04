0873f70e97bf7e54d0cc0ffbf2c91584004e0a95
yishoka <63327438+yishoka@users.noreply.github.com>
2020-05-04 21:20:33 +0800

    作業02_編蝠與史萊姆互打

---
+++ Homework_02_01/Assets/Bat.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Bat : MonoBehaviour
+{
+    public int HP;
+    public int ATK;
+    public int Heal;
+    public Text TextResult;
+    public Slime Slime1;
+
+    public void Start()
+    {
+        HP = 200;
+        ATK = 20;
+        Heal = 10;
+    }
+    
+    public void Attack()
+    {
+        Slime1.Hurt(ATK);
+    }
+
+    public void Hurt(int EnemyATK)
+    {
+        HP -= EnemyATK;
+        TextResult.text = gameObject.name + " - 受到傷害： " + EnemyATK + "\n" + gameObject.name + " - 血量剩下：" + HP;
+    }
+
+    public void Healing()
+    {
+        HP += Heal;
+        TextResult.text = gameObject.name + " - 受到治癒： " + Heal + "\n" + gameObject.name + " - 血量剩下：" + HP;
+    }
+}
---
+++ Homework_02_01/Assets/Slime.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Slime : MonoBehaviour
+{
+    public int HP;
+    public int ATK;
+    public int Heal;
+    public Text TextResult;
+    public Bat Bat1;
+
+    public void Start()
+    {
+        HP = 100;
+        ATK = 10;
+        Heal = 30;
+    }
+
+    public void Attack()
+    {
+        Bat1.Hurt(ATK);
+    }
+
+    public void Hurt(int EnemyATK)
+    {
+        HP -= EnemyATK;
+        TextResult.text = gameObject.name + " - 受到傷害： " + EnemyATK + "\n" + gameObject.name + " - 血量剩下：" + HP;
+    }
+
+    public void Healing()
+    {
+        HP += Heal;
+        TextResult.text = gameObject.name + " - 受到治癒： " + Heal + "\n" + gameObject.name + " - 血量剩下：" + HP;
+    }
+}
